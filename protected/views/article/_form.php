5f5a117e55248dcc514b97ef58f2fdedb28f868d
Alexander Randa <or10n@tut.by>
2010-12-09 20:47:44 +0200

    new editor

--- protected/views/article/_form.php
+++ protected/views/article/_form.php
@@ -1,7 +1,24 @@
 <!-- TinyMCE -->
 <script type="text/javascript" src="/js/tiny_mce/tiny_mce.js"></script>
 <script type="text/javascript">
-  tinyMCE.init({
+tinyMCE.init({
+	mode : "textareas", language : "ru",
+	theme : "advanced", skin : "o2k7",
+	width: "910px", height : "400px",
+	plugins : "imagemanager,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave",
+	theme_advanced_buttons1 : "code,newdocument,|,bold,italic,underline,strikethrough,|,sub,sup,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontselect,fontsizeselect,|,visualchars,visualaid,|,fullscreen",
+	theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,nonbreaking,template,pagebreak,|,charmap,media,advhr,|,link,unlink,anchor,image,insertimage",
+	theme_advanced_buttons3 : "tablecontrols,|,removeformat,|,insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+	theme_advanced_toolbar_location : "top",
+	theme_advanced_toolbar_align : "left",
+	theme_advanced_statusbar_location : "bottom",
+	theme_advanced_resizing : true,
+	extended_valid_elements : 'script[type|src],iframe[src|style|width|height|scrolling|marginwidth|marginheight|frameborder],div[*],p[*],object[width|height|classid|codebase|embed|param],param[name|value],embed[param|src|type|width|height|flashvars|wmode]',
+	relative_urls : false, // абсолютные URL для MCFileManager
+	remove_script_host : false, // Не удалять имя хоста из URL для MCFileManager
+	convert_urls : false
+});
+/*  tinyMCE.init({
     // General options
     mode : "textareas",
     theme : "advanced",
@@ -33,7 +50,7 @@
       username : "Some User",
       staffid : "991234"
     }
-  });
+  });*/
 </script>
 <!-- /TinyMCE -->
 
@@ -55,45 +72,45 @@
     // это быдло код. сюда лучше не смотреть :(
     $titles = CHtml::listData(Contents::model()->findAll(), 'id','title');
     $parents = CHtml::listData(Contents::model()->findAll(), 'id','parent');
-    
+
     $input = $parents;
-    
+
     global $last;
     global $titles;
     global $list;
-    
+
     $pref = '';
-    foreach($input as $key=>$value){    
+    foreach($input as $key=>$value){
         if(@!$last[$key]){
-          @$list[$key] = "{$titles[$key]}"; 
+          @$list[$key] = "{$titles[$key]}";
           tree($input,$key,$pref,$list);
         }
     }
-    
+
     function tree($input,$parent,$pref,$list){
-      
+
       global $last;
       global $list;
-      
+
       if($parent != 0){
       $dir = $pref . ' *';
       $last[$parent] = 1;
-      
+
       foreach($input as $key=>$value){
         if($value == $parent){
           $list[$key] = "$dir " . @$titles[$key];
-          tree($input,$key,$pref. ' *',$list);      
-        } 
+          tree($input,$key,$pref. ' *',$list);
+        }
       }
       }
-      
 
-    }     
+
+    }
     foreach(CHtml::listData(Contents::model()->findAll(), 'id','title') as $key=>$value){
-      $list[$key] = $list[$key] . ' ' . $value; 
-    }     
-		echo $form->dropDownList($model,'content_id', $list ); 
-		 
+      $list[$key] = $list[$key] . ' ' . $value;
+    }
+		echo $form->dropDownList($model,'content_id', $list );
+
 		?>
 
 		<?php echo $form->error($model,'content_id'); ?>
