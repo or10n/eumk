4d5a5dda61ddabdd26e3fccd0f2c4be76d86dd80
Alexander Randa <or10n@tut.by>
2011-04-24 22:52:02 +0300

    update

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -14,13 +14,13 @@ $this->pageTitle=Yii::app()->name;
 			</a></em>
 		</td>
 		<td width=25% align=center>
-			<em><a href="/index.php?r=contents&type=1">
+			<em><a href="index.php?r=contents&type=1">
 				<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/theory_gray.jpg" id="theory" width="220" height="270" border="0" onMouseOver="javascript:change_color('theory')" onMouseOut="javascript:change_gray('theory')">
 				<center>Теория по курсу</center>
 			</a></em>
 		</td>
 		<td width=25% align=center>
-			<em><a href="/index.php?r=contents&type=3">
+			<em><a href="index.php?r=contents&type=3">
 				<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/practice_gray.jpg" id="practice" width="220" height="270" border="0" onMouseOver="javascript:change_color('practice')" onMouseOut="javascript:change_gray('practice')">
 				<center>Практика</center>
 			</a></em>
